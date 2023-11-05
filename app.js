7db1cce3fff6bb59d009b7758e0cb1af53c8ebf9
junayed-code <junayed.code@gmail.com>
2023-11-05 23:36:07 +0600

    Add JSON parser and cors policy middlewares

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import "./mongoose/init.js"; // Initialize mongoose
+import cors from "cors";
 import express from "express";
 import errorHandler from "./middlewares/errorHandler.js";
 import notFound from "./middlewares/notFound.js";
@@ -9,6 +10,12 @@ const { PORT = 4040 } = process.env;
 
 // Initialize express app
 const app = express();
+
+// JSON content parser middleware
+app.use(express.json());
+
+// CORS policy handler middleware
+app.use(cors({ origin: [], credentials: true }));
 
 // Router middlewares
 app.use("/categories", CategoriesRoute);
